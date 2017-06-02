1358ac46d073903f0221bb170f0fdcf27045c930
Lionel Villard <villard@us.ibm.com>
2017-06-02 11:34:05 -0400

    Add login-redirect web-exported sequence action.

--- actions/login/login.js
+++ actions/login/login.js
@@ -10,124 +10,124 @@ var request = require('request');
  */
 function main(params) {
     return new Promise((resolve, reject) => {
-	// here are the provider secrets
-	const providers = params.providers;
-    
-	// here is the oauth code the client gave us
-	const code = params.code;
+        // here are the provider secrets
+        const providers = params.providers;
 
-	let state;
-	try {
-	    state = JSON.parse(params.state)
-	} catch (e) {
-	    console.log('Error parsing state', state)
-	}
-	
-	var providerName = params.provider || params.providerName || state.providerName;
-	console.log(`providerName=${providerName}`);
-	var provider = providers[providerName];
+        // here is the oauth code the client gave us
+        const code = params.code;
 
-	//
-	// this is the body of our access_token request
-	//
-	var form = {
-	    client_id: provider.credentials.client_id,
-	    client_secret: provider.credentials.client_secret,
-	    code: code
-	};
-	const params_token_endpoint_form = params.token_endpoint_form || {};
-	if (provider.token_endpoint_form) {
-	    for (var x in provider.token_endpoint_form) {
-		form[x] = params_token_endpoint_form[x] || provider.token_endpoint_form[x];
-	    }
-	}
+        let state;
+        try {
+            state = JSON.parse(params.state)
+        } catch (e) {
+            console.log('Error parsing state', state)
+        }
 
-	//
-	// form the request options for the access_token
-	//
-	var options = {
-	    url: provider.endpoints.token,
-	    method: 'POST',
-	    headers: {
-		'Content-Type': 'application/json'
-	    }
-	};
-	if (provider.token_endpoint_form_is_json) {
-	    options.headers['Accept'] = 'application/json';
-	    options.body = form;
-	    options.json = true;
-	} else {
-	    options.form = form;
-	}
+        var providerName = params.provider || params.providerName || state.providerName;
+        console.log(`providerName=${providerName}`);
+        var provider = providers[providerName];
 
-	if (provider.token_endpoint_needs_auth) {
-	    options.auth = {
-		user: form.client_id,
-		pass: form.client_secret
-	    }
-	}
+        //
+        // this is the body of our access_token request
+        //
+        var form = {
+            client_id: provider.credentials.client_id,
+            client_secret: provider.credentials.client_secret,
+            code: code
+        };
+        const params_token_endpoint_form = params.token_endpoint_form || {};
+        if (provider.token_endpoint_form) {
+            for (var x in provider.token_endpoint_form) {
+                form[x] = params_token_endpoint_form[x] || provider.token_endpoint_form[x];
+            }
+        }
 
-	//
-	// ok, here we go, exchanging the oauth code for an access_token
-	//
-	request(options, function(err, response, body) {
-	    if (err || response.statusCode >= 400) {
-		const errorMessage = err || { statusCode: response.statusCode, body: body }
-		console.error(JSON.stringify(errorMessage));
-		reject(errorMessage);
+        //
+        // form the request options for the access_token
+        //
+        var options = {
+            url: provider.endpoints.token,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        if (provider.token_endpoint_form_is_json) {
+            options.headers['Accept'] = 'application/json';
+            options.body = form;
+            options.json = true;
+        } else {
+            options.form = form;
+        }
 
-	    } else {
-		//
-		// all right, we now have an access_token
-		//
-		if (typeof body == 'string') {
-		    body = JSON.parse(body);
-		}
+        if (provider.token_endpoint_needs_auth) {
+            options.auth = {
+                user: form.client_id,
+                pass: form.client_secret
+            }
+        }
 
-		// console.log("TOKEN RESPONSE", body)
+        //
+        // ok, here we go, exchanging the oauth code for an access_token
+        //
+        request(options, function (err, response, body) {
+            if (err || response.statusCode >= 400) {
+                const errorMessage = err || {statusCode: response.statusCode, body: body}
+                console.error(JSON.stringify(errorMessage));
+                reject(errorMessage);
 
-		//
-		// now we request the user's profile, so that we have some
-		// persistent identifying information; e.g. email address
-		// for account handle
-		//
-		request({
-		    url: provider.endpoints.userinfo + (provider.token_as_query ? `?${provider.token_as_query}=${body.access_token}` : ''),
-		    method: 'GET',
-		    headers: {
-			'Accept': 'application/json',
-			'Authorization': (provider.authorization_type || 'token') + ' ' + body.access_token,
-			'User-Agent': 'OpenWhisk'
-		    }
-		}, function(err2, response2, body2) {
-		    if (err2 || response2.statusCode >= 400) {
-			const errorMessage = err2 || { statusCode: response2.statusCode, body: body2 }
-			console.error(JSON.stringify(errorMessage));
-			reject(errorMessage);
+            } else {
+                //
+                // all right, we now have an access_token
+                //
+                if (typeof body == 'string') {
+                    body = JSON.parse(body);
+                }
 
-		    } else {
-			//
-			// great, now we have the profile!
-			//
-			if (typeof body2 == 'string') {
-			    body2 = JSON.parse(body2);
-			}
+                // console.log("TOKEN RESPONSE", body)
 
-			// console.log("Profile Response", body2)
+                //
+                // now we request the user's profile, so that we have some
+                // persistent identifying information; e.g. email address
+                // for account handle
+                //
+                request({
+                    url: provider.endpoints.userinfo + (provider.token_as_query ? `?${provider.token_as_query}=${body.access_token}` : ''),
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Authorization': (provider.authorization_type || 'token') + ' ' + body.access_token,
+                        'User-Agent': 'OpenWhisk'
+                    }
+                }, function (err2, response2, body2) {
+                    if (err2 || response2.statusCode >= 400) {
+                        const errorMessage = err2 || {statusCode: response2.statusCode, body: body2}
+                        console.error(JSON.stringify(errorMessage));
+                        reject(errorMessage);
 
-			resolve({
-			    provider: providerName,
-			    access_token: body.access_token,
-			    id: body2[provider.userinfo_identifier],
-			    idRecord: provider.userinfo_identifier_full_record && body2,
+                    } else {
+                        //
+                        // great, now we have the profile!
+                        //
+                        if (typeof body2 == 'string') {
+                            body2 = JSON.parse(body2);
+                        }
 
-			    access_token_body: body,
-			    state: state
-			});
-		    }
-		});
-	    }
-	});
+                        // console.log("Profile Response", body2)
+
+                        resolve({
+                            provider: providerName,
+                            access_token: body.access_token,
+                            id: body2[provider.userinfo_identifier],
+                            idRecord: provider.userinfo_identifier_full_record && body2,
+
+                            access_token_body: body,
+                            state: state
+                        });
+                    }
+                });
+            }
+        });
     });
 }
 
--- actions/redirect/redirect.js
+++ actions/redirect/redirect.js
@@ -1,24 +1,32 @@
+/*
+ @param {Object} params
+ @param {Object} params.state
+ @param {string} [params.state.redirect_uri] - target location
+ @param {string} [params.session_cookie]     - name of the session cookie.
+ */
 function main(params) {
     if (params.state && params.state.redirect_uri) {
-	const payload = {
-	    code: 302,
-	    headers: {
-		location: params.state.redirect_uri
-	    }
-	}
+        // TODO: should only allow some URI for security reason.
 
-	if (params.session_cookie) {
-	    const cookie = {
-		provider: params.provider,
-		access_token: params.access_token,
-		id: params.id,
-		idRecord: params.idRecord
-	    }
-	    payload.headers["Set-Cookie"] = `${params.session_cookie}=${JSON.stringify(cookie)}; Expires=${new Date(new Date().getTime()+86409000).toUTCString()}; Path=/; Version=`
-	}
+        const payload = {
+            statusCode: 302,
+            headers: {
+                location: params.state.redirect_uri
+            }
+        };
 
-        return payload
+        if (params.session_cookie) {
+            const cookie = {
+                provider: params.provider,
+                access_token: params.access_token,
+                id: params.id,
+                idRecord: params.idRecord
+            };
+            payload.headers["Set-Cookie"] = `${params.session_cookie}=${JSON.stringify(cookie)}; Path=/;`
+        }
+
+        return payload;
     } else {
-	return params
+        return params;
     }
 }
